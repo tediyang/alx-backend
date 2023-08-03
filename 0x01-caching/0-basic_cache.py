f77e45e261fa4717e01f91e54fb6df3fbcbd6cd4
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-04 00:10:18 +0100

    basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,17 +10,36 @@ class BasicCache(BaseCaching):
     """
         A Class that implements a basic caching system
 
-    Args:
-        BaseCaching (class): Base cache model
+        Args:
+            BaseCaching (class): Base cache model
     """
     def __init__(self) -> None:
+        """
+            initialize the base
+        """
         super().__init__()
 
     def put(self, key: str, item: Any) -> None:
+        """
+            add data to the cache dict.
+
+            Args:
+                key (str): string key
+                item (Any): any data type
+        """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key: str) -> Optional[Any]:
+        """
+            get the data with the provided key.
+
+            Args:
+                key (str): the key
+
+        Returns:
+            Optional[Any]: value with any data type
+        """
         if key:
             return self.cache_data.get(key)
         return None
