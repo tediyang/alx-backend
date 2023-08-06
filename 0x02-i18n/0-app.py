85624391e56bf5359007b2b1487427a737505f3f
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-08-06 17:49:50 +0100

    removed jinja method and added the string directly

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
+"""
+    Basic flask setup
+"""
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    title = "Welcome to Holberton"
-    h1 = "Hello world"
-    return render_template('./0-index.html', title=title, h1=h1)
+    return render_template('./0-index.html')
 
 
 if __name__ == "__main__":
